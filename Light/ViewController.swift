98178211634bf5989ffebe9cde126fcd6de0150c
Dennis Zimmermann <dennzimm@icloud.com>
2017-05-27 12:34:09 +0200

    Update viewDidLoad function

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -36,6 +36,7 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    updateUI()
   }
 
   override func didReceiveMemoryWarning() {
