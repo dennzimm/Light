b6985f1c878f32926389e53b2273a2d692f014b4
Dennis Zimmermann <dennzimm@icloud.com>
2017-05-27 12:33:11 +0200

    Update buttonPressed function

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
   
+  @IBOutlet weak var lightButton: UIButton!
+  
   var lightOn = true
   
   
@@ -22,9 +24,11 @@ class ViewController: UIViewController {
   func updateUI() {
     if lightOn {
       view.backgroundColor = .white
+      lightButton.setTitle("Off", for: .normal)
       
     } else {
       view.backgroundColor = .black
+      lightButton.setTitle("On", for: .normal)
       
     }
   }
